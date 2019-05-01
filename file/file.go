9e7adb09a3c615cabee7eb18242eb4741550b5e6
zppro <zpprozppro@gmail.com>
2019-05-01 20:53:07 +0800

    add GetAllFileExtInfo

--- file/file.go
+++ file/file.go
@@ -10,6 +10,50 @@ import (
 	"regexp"
 )
 
+type ExtInfo struct {
+	FI os.FileInfo
+	Path string
+	Reg *regexp.Regexp
+}
+func (fei *ExtInfo) Name () string {
+	return fei.FI.Name()
+}
+func (fei *ExtInfo) FullName () string {
+	return fmt.Sprintf("%s/%s", fei.Path, fei.FI.Name())
+}
+
+func (fei *ExtInfo) Hash () string {
+	return GetFileHash(fei.FullName())
+}
+
+func GetAllFileExtInfo(path string, patterns []string) (extInfos []ExtInfo) {
+	entries, err := ioutil.ReadDir(path)
+	if err != nil {
+		fmt.Printf("ReadDir error %s\n", err)
+		return
+	}
+
+	for _, fi := range entries {
+		filename := fi.Name()
+		if fi.IsDir() {
+			fmt.Printf("dir [%s]\n", path+"/"+filename)
+			extInfos = append(extInfos, GetAllFileExtInfo(path+fi.Name()+"/", patterns)...)
+		} else {
+			if len(patterns) > 0 {
+				for _, pattern := range patterns {
+					if ok, _ := regexp.MatchString(pattern, filename); ok {
+						reg := regexp.MustCompile(pattern)
+						extInfos = append(extInfos, ExtInfo{FI: fi, Path: path, Reg: reg})
+					}
+				}
+			} else {
+				extInfos = append(extInfos, ExtInfo{FI: fi, Path: path})
+			}
+		}
+	}
+	return
+}
+
 func GetAllFile(path string, patterns []string) (files []string) {
 	entries, err := ioutil.ReadDir(path)
 	if err != nil {
@@ -25,9 +69,7 @@ func GetAllFile(path string, patterns []string) (files []string) {
 		} else {
 			for _, pattern := range patterns {
 				if ok,_ := regexp.MatchString(pattern, filename); ok {
-					reg := regexp.MustCompile(pattern)
-					files = append(files, reg.FindAllString(filename, -1)...)
-					//fmt.Printf("dev file %q, %d\n", files, len(files))
+					files = append(files, filename)
 				}
 			}
 		}
